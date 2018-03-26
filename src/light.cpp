32756ff67873089b91b4dc24993360a1517c6394
Zunawe <gyroscope15@gmail.com>
2018-03-26 16:30:35 -0600

    Fix light methods not returning a value

--- src/light.cpp
+++ src/light.cpp
@@ -8,10 +8,12 @@
  * returned by OpenGL
  */
 bool Light::setUniforms(Shader shader, std::string lightLocation) const{
-	shader.setUniform(lightLocation + ".position", position);
-	shader.setUniform(lightLocation + ".ambient", ambient);
-	shader.setUniform(lightLocation + ".diffuse", diffuse);
-	shader.setUniform(lightLocation + ".specular", specular);
+	bool allValid = true;
+	allValid = allValid && shader.setUniform(lightLocation + ".position", position);
+	allValid = allValid && shader.setUniform(lightLocation + ".ambient", ambient);
+	allValid = allValid && shader.setUniform(lightLocation + ".diffuse", diffuse);
+	allValid = allValid && shader.setUniform(lightLocation + ".specular", specular);
+	return allValid;
 }
 
 /**
@@ -22,10 +24,12 @@ bool Light::setUniforms(Shader shader, std::string lightLocation) const{
  * returned by OpenGL
  */
 bool PointLight::setUniforms(Shader shader, std::string lightLocation) const{
-	Light::setUniforms(shader, lightLocation);
-	shader.setUniform(lightLocation + ".kc", kc);
-	shader.setUniform(lightLocation + ".kl", kl);
-	shader.setUniform(lightLocation + ".kq", kq);
+	bool allValid = true;
+	allValid = allValid && Light::setUniforms(shader, lightLocation);
+	allValid = allValid && shader.setUniform(lightLocation + ".kc", kc);
+	allValid = allValid && shader.setUniform(lightLocation + ".kl", kl);
+	allValid = allValid && shader.setUniform(lightLocation + ".kq", kq);
+	return allValid;
 }
 
 /**
@@ -36,7 +40,9 @@ bool PointLight::setUniforms(Shader shader, std::string lightLocation) const{
  * returned by OpenGL
  */
 bool SpotLight::setUniforms(Shader shader, std::string lightLocation) const{
-	Light::setUniforms(shader, lightLocation);
-	shader.setUniform(lightLocation + ".direction", direction);
-	shader.setUniform(lightLocation + ".cosAngle", cosAngle);
+	bool allValid = true;
+	allValid = allValid && Light::setUniforms(shader, lightLocation);
+	allValid = allValid && shader.setUniform(lightLocation + ".direction", direction);
+	allValid = allValid && shader.setUniform(lightLocation + ".cosAngle", cosAngle);
+	return allValid;
 }
